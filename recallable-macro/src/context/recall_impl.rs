0e7570e4f00fe3ad796a17bf94fcdc0b66caea0a
Lan, Jian <shapeless.cat@gmail.com>
2026-03-24 19:12:45 +0800

    refactor: add free functions to recallable_impl, recall_impl, from_impl using StructIr + CodegenEnv

--- recallable-macro/src/context/recall_impl.rs
+++ recallable-macro/src/context/recall_impl.rs
@@ -2,6 +2,7 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 
 use crate::context::MacroContext;
+use crate::context::{CodegenEnv, FieldIr, FieldMember, FieldStrategy, RecallPath, StructIr};
 
 impl<'a> MacroContext<'a> {
     // ============================================================
@@ -45,5 +46,63 @@ impl<'a> MacroContext<'a> {
             });
 
         quote! { #(#statements)* }
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) fn gen_recall_impl(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
+    let recall_trait = &env.recall_trait;
+    let (impl_generics, type_generics, _) = ir.generics.split_for_impl();
+    let extra_bounds = ir.recallable_bounds(recall_trait);
+    let where_clause = ir.extend_where_clause(&extra_bounds);
+    let struct_name = ir.name;
+
+    let memento_fields: Vec<_> = ir.memento_fields().collect();
+
+    let recall_param_name = if memento_fields.is_empty() {
+        quote! { _memento }
+    } else {
+        quote! { memento }
+    };
+
+    let statements = memento_fields
+        .iter()
+        .map(|field| build_recall_statement_ir(field, recall_trait));
+
+    quote! {
+        impl #impl_generics #recall_trait
+            for #struct_name #type_generics
+        #where_clause {
+            #[inline(always)]
+            fn recall(&mut self, #recall_param_name: Self::Memento) {
+                #(#statements)*
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn build_recall_statement_ir(field: &FieldIr, recall_trait: &TokenStream2) -> TokenStream2 {
+    let member = &field.member;
+    let memento_index = field
+        .memento_index
+        .expect("memento_fields() guarantees memento_index is Some");
+
+    let memento_member = match member {
+        FieldMember::Named(name) => quote! { #name },
+        FieldMember::Unnamed(_) => {
+            let idx = syn::Index::from(memento_index);
+            quote! { #idx }
+        }
+    };
+
+    match &field.strategy {
+        FieldStrategy::StoreAsSelf => {
+            quote! { self.#member = memento.#memento_member; }
+        }
+        FieldStrategy::StoreAsMemento(RecallPath::WholeType) => {
+            quote! { #recall_trait::recall(&mut self.#member, memento.#memento_member); }
+        }
+        FieldStrategy::Skip => unreachable!("memento_fields() filters skipped fields"),
     }
 }
--- recallable-macro/src/context/recallable_impl.rs
+++ recallable-macro/src/context/recallable_impl.rs
@@ -2,6 +2,7 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 
 use crate::context::MacroContext;
+use crate::context::{CodegenEnv, StructIr};
 
 impl<'a> MacroContext<'a> {
     // ============================================================
@@ -22,6 +23,24 @@ impl<'a> MacroContext<'a> {
             #where_clause {
                 type Memento = #memento_struct_type;
             }
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) fn gen_recallable_impl(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
+    let (impl_generics, type_generics, _) = ir.generics.split_for_impl();
+    let recallable_trait = &env.recallable_trait;
+    let struct_name = ir.name;
+    let extra_bounds = ir.recallable_bounds(recallable_trait);
+    let where_clause = ir.extend_where_clause(&extra_bounds);
+    let memento_type = ir.memento_type();
+
+    quote! {
+        impl #impl_generics #recallable_trait
+            for #struct_name #type_generics
+        #where_clause {
+            type Memento = #memento_type;
         }
     }
 }
