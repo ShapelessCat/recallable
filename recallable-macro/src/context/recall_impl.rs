a50f6284ed81cc23cd4cc133fadba4e2b28a3adf
Lan, Jian <shapeless.cat@gmail.com>
2026-03-25 01:08:35 +0800

    refactor(macro): simplify Recall impl emission

--- recallable-macro/src/context/recall_impl.rs
+++ recallable-macro/src/context/recall_impl.rs
@@ -1,28 +1,13 @@
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
+use syn::WherePredicate;
 
 use crate::context::{CodegenEnv, FieldIr, FieldMember, FieldStrategy, RecallPath, StructIr};
 
 pub(crate) fn gen_recall_impl(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
     let recall_trait = &env.recall_trait;
-    let recallable_trait = &env.recallable_trait;
     let impl_generics = ir.impl_generics();
-    let memento_trait_bounds = quote! {
-        ::core::clone::Clone
-            + ::core::fmt::Debug
-            + ::core::cmp::PartialEq
-    };
-    let where_clause = {
-        let mut extra_bounds = ir.recallable_bounds(recall_trait);
-        extra_bounds.extend(ir.recallable_memento_bounds(&memento_trait_bounds));
-        extra_bounds.extend(ir.whole_type_bounds(recall_trait));
-        extra_bounds.extend(ir.whole_type_memento_bounds(recallable_trait, &memento_trait_bounds));
-        if env.serde_enabled {
-            let deserialize_owned = quote! { ::serde::de::DeserializeOwned };
-            extra_bounds.extend(ir.whole_type_memento_bounds(recallable_trait, &deserialize_owned));
-        }
-        ir.extend_where_clause(&extra_bounds)
-    };
+    let where_clause = build_recall_where_clause(ir, env);
     let struct_type = ir.struct_type();
     let recall_method = build_recall_method(ir, recall_trait);
 
@@ -35,15 +20,30 @@ pub(crate) fn gen_recall_impl(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
     }
 }
 
+fn build_recall_where_clause(ir: &StructIr, env: &CodegenEnv) -> Option<syn::WhereClause> {
+    let extra_bounds = collect_recall_bounds(ir, env);
+    ir.extend_where_clause(&extra_bounds)
+}
+
+fn collect_recall_bounds(ir: &StructIr, env: &CodegenEnv) -> Vec<WherePredicate> {
+    let recall_trait = &env.recall_trait;
+    let recallable_trait = &env.recallable_trait;
+    let memento_trait_bounds = env.memento_trait_bounds();
+
+    let mut bounds = ir.recallable_bounds(recall_trait);
+    bounds.extend(ir.recallable_memento_bounds(&memento_trait_bounds));
+    bounds.extend(ir.whole_type_bounds(recall_trait));
+    bounds.extend(ir.whole_type_memento_bounds(recallable_trait, &memento_trait_bounds));
+    if let Some(deserialize_owned) = env.deserialize_owned_bound() {
+        bounds.extend(ir.whole_type_memento_bounds(recallable_trait, &deserialize_owned));
+    }
+
+    bounds
+}
+
 fn build_recall_method(ir: &StructIr, recall_trait: &TokenStream2) -> TokenStream2 {
     let memento_fields: Vec<_> = ir.memento_fields().collect();
-
-    let recall_param_name = if memento_fields.is_empty() {
-        quote! { _memento }
-    } else {
-        quote! { memento }
-    };
-
+    let recall_param_name = build_recall_param_name(&memento_fields);
     let statements = memento_fields
         .iter()
         .map(|field| build_recall_statement(field, recall_trait));
@@ -56,19 +56,17 @@ fn build_recall_method(ir: &StructIr, recall_trait: &TokenStream2) -> TokenStrea
     }
 }
 
+fn build_recall_param_name(memento_fields: &[&FieldIr]) -> TokenStream2 {
+    if memento_fields.is_empty() {
+        quote! { _memento }
+    } else {
+        quote! { memento }
+    }
+}
+
 fn build_recall_statement(field: &FieldIr, recall_trait: &TokenStream2) -> TokenStream2 {
     let member = &field.member;
-    let memento_index = field
-        .memento_index
-        .expect("memento_fields() guarantees memento_index is Some");
-
-    let memento_member = match member {
-        FieldMember::Named(name) => quote! { #name },
-        FieldMember::Unnamed(_) => {
-            let idx = syn::Index::from(memento_index);
-            quote! { #idx }
-        }
-    };
+    let memento_member = build_memento_member(field);
 
     match &field.strategy {
         FieldStrategy::StoreAsSelf => {
@@ -78,5 +76,19 @@ fn build_recall_statement(field: &FieldIr, recall_trait: &TokenStream2) -> Token
             quote! { #recall_trait::recall(&mut self.#member, memento.#memento_member); }
         }
         FieldStrategy::Skip => unreachable!("memento_fields() filters skipped fields"),
+    }
+}
+
+fn build_memento_member(field: &FieldIr) -> TokenStream2 {
+    let member = &field.member;
+    match member {
+        FieldMember::Named(name) => quote! { #name },
+        FieldMember::Unnamed(_) => {
+            let memento_index = field
+                .memento_index
+                .expect("memento_fields() guarantees memento_index is Some");
+            let idx = syn::Index::from(memento_index);
+            quote! { #idx }
+        }
     }
 }
