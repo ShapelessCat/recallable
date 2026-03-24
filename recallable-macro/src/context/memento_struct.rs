73f1f623442cca231b3cf382468050d265d27913
Lan, Jian <shapeless.cat@gmail.com>
2026-03-25 01:07:41 +0800

    refactor(macro): simplify memento struct emission

--- recallable-macro/src/context/memento_struct.rs
+++ recallable-macro/src/context/memento_struct.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use std::collections::HashSet;
-use syn::Ident;
+use syn::{Ident, WhereClause, WherePredicate};
 
 use crate::context::{
     CodegenEnv, FieldIr, FieldMember, FieldStrategy, RecallPath, StructIr, StructShape,
@@ -9,63 +9,88 @@ use crate::context::{
 };
 
 pub(crate) fn gen_memento_struct(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
-    let derives = if env.serde_enabled {
-        quote! { #[derive(::core::clone::Clone, ::core::fmt::Debug, ::core::cmp::PartialEq, ::serde::Deserialize)] }
-    } else {
-        quote! { #[derive(::core::clone::Clone, ::core::fmt::Debug, ::core::cmp::PartialEq)] }
-    };
-
+    let derives = build_memento_derives(env);
     let memento_name = ir.memento_name();
     let memento_generics = ir.memento_decl_generics();
-    let recallable_trait = &env.recallable_trait;
-    let memento_trait_bounds = quote! {
-        ::core::clone::Clone
-            + ::core::fmt::Debug
-            + ::core::cmp::PartialEq
-    };
-    let mut bounded_types = ir.recallable_bounds(recallable_trait);
-    bounded_types.extend(ir.recallable_memento_bounds(&memento_trait_bounds));
-    bounded_types.extend(ir.whole_type_bounds(recallable_trait));
-    bounded_types.extend(ir.whole_type_memento_bounds(recallable_trait, &memento_trait_bounds));
-    if env.serde_enabled {
-        let deserialize_owned = quote! { ::serde::de::DeserializeOwned };
-        bounded_types.extend(ir.whole_type_memento_bounds(recallable_trait, &deserialize_owned));
-    }
-    let mut where_clause = ir
-        .memento_where_clause()
-        .cloned()
-        .unwrap_or_else(|| syn::parse_quote! { where });
-    if bounded_types.is_empty() && where_clause.predicates.is_empty() {
-        where_clause.predicates.clear();
-    } else {
-        where_clause.predicates.extend(bounded_types);
-    }
-    let where_clause = (!where_clause.predicates.is_empty()).then_some(where_clause);
-
-    let generic_type_params: HashSet<&Ident> = ir.type_params().map(|p| &p.ident).collect();
-
-    let mut fields: Vec<_> = ir
-        .memento_fields()
-        .map(|field| build_memento_field(field, recallable_trait, &generic_type_params))
-        .collect();
-    if let Some(marker_ty) = ir.synthetic_marker_type() {
-        fields.push(build_marker_field(
-            &marker_ty,
-            ir.generated_memento_shape(),
-            env,
-        ));
-    }
-
-    let body = match ir.generated_memento_shape() {
-        StructShape::Named => quote! { #where_clause { #(#fields),* } },
-        StructShape::Unnamed => quote! { ( #(#fields),* ) #where_clause; },
-        StructShape::Unit => quote! { #where_clause; },
-    };
+    let body = build_memento_body(ir, env);
 
     quote! {
         #derives
         pub struct #memento_name #memento_generics #body
     }
+}
+
+fn build_memento_derives(env: &CodegenEnv) -> TokenStream2 {
+    if env.serde_enabled {
+        quote! { #[derive(::core::clone::Clone, ::core::fmt::Debug, ::core::cmp::PartialEq, ::serde::Deserialize)] }
+    } else {
+        quote! { #[derive(::core::clone::Clone, ::core::fmt::Debug, ::core::cmp::PartialEq)] }
+    }
+}
+
+fn build_memento_body(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
+    let shape = ir.generated_memento_shape();
+    let where_clause = build_memento_where_clause(ir, env);
+    let fields = collect_memento_fields(ir, env, shape);
+
+    match shape {
+        StructShape::Named => quote! { #where_clause { #(#fields),* } },
+        StructShape::Unnamed => quote! { ( #(#fields),* ) #where_clause; },
+        StructShape::Unit => quote! { #where_clause; },
+    }
+}
+
+fn build_memento_where_clause(ir: &StructIr, env: &CodegenEnv) -> Option<WhereClause> {
+    let mut where_clause = ir
+        .memento_where_clause()
+        .cloned()
+        .unwrap_or_else(|| syn::parse_quote! { where });
+    let bounded_types = collect_memento_bounds(ir, env);
+
+    if bounded_types.is_empty() && where_clause.predicates.is_empty() {
+        return None;
+    }
+
+    where_clause.predicates.extend(bounded_types);
+    Some(where_clause)
+}
+
+fn collect_memento_bounds(ir: &StructIr, env: &CodegenEnv) -> Vec<WherePredicate> {
+    let recallable_trait = &env.recallable_trait;
+    let memento_trait_bounds = env.memento_trait_bounds();
+
+    let mut bounds = ir.recallable_bounds(recallable_trait);
+    bounds.extend(ir.recallable_memento_bounds(&memento_trait_bounds));
+    bounds.extend(ir.whole_type_bounds(recallable_trait));
+    bounds.extend(ir.whole_type_memento_bounds(recallable_trait, &memento_trait_bounds));
+    if let Some(deserialize_owned) = env.deserialize_owned_bound() {
+        bounds.extend(ir.whole_type_memento_bounds(recallable_trait, &deserialize_owned));
+    }
+
+    bounds
+}
+
+fn collect_memento_fields(
+    ir: &StructIr,
+    env: &CodegenEnv,
+    shape: StructShape,
+) -> Vec<TokenStream2> {
+    let recallable_trait = &env.recallable_trait;
+    let generic_type_params = collect_generic_type_params(ir);
+    let mut fields: Vec<_> = ir
+        .memento_fields()
+        .map(|field| build_memento_field(field, recallable_trait, &generic_type_params))
+        .collect();
+
+    if let Some(marker_ty) = ir.synthetic_marker_type() {
+        fields.push(build_marker_field(&marker_ty, shape, env));
+    }
+
+    fields
+}
+
+fn collect_generic_type_params<'a>(ir: &'a StructIr<'a>) -> HashSet<&'a Ident> {
+    ir.type_params().map(|param| &param.ident).collect()
 }
 
 fn build_marker_field(
@@ -75,7 +100,7 @@ fn build_marker_field(
 ) -> TokenStream2 {
     let serde_attr = env
         .serde_enabled
-        .then(|| quote! { #[serde(skip, default)] });
+        .then_some(quote! { #[serde(skip, default)] });
 
     match shape {
         StructShape::Named => quote! {
