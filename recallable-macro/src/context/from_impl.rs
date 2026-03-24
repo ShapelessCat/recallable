78d3675fdf1ddb5592d6e60a0b3a05535757813d
Lan, Jian <shapeless.cat@gmail.com>
2026-03-24 19:12:45 +0800

    refactor: add free functions to recallable_impl, recall_impl, from_impl using StructIr + CodegenEnv

--- recallable-macro/src/context/from_impl.rs
+++ recallable-macro/src/context/from_impl.rs
@@ -3,6 +3,7 @@ use quote::quote;
 use syn::{Fields, WherePredicate, parse_quote};
 
 use crate::context::MacroContext;
+use crate::context::{CodegenEnv, FieldIr, FieldStrategy, RecallPath, StructIr, StructShape};
 
 impl<'a> MacroContext<'a> {
     // ======================================================================
@@ -66,4 +67,72 @@ impl<'a> MacroContext<'a> {
             .collect();
         self.extend_where_clause(&trait_bounds)
     }
+}
+
+#[allow(dead_code)]
+pub(crate) fn gen_from_impl(ir: &StructIr, env: &CodegenEnv) -> TokenStream2 {
+    let (impl_generics, type_generics, _) = ir.generics.split_for_impl();
+    let where_clause = build_from_where_clause(ir, env);
+    let struct_name = ir.name;
+    let memento_type = ir.memento_type();
+    let body = build_from_body_ir(ir);
+
+    quote! {
+        impl #impl_generics ::core::convert::From<#struct_name #type_generics>
+            for #memento_type
+        #where_clause {
+            #[inline(always)]
+            fn from(value: #struct_name #type_generics) -> Self {
+                #body
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn build_from_body_ir(ir: &StructIr) -> TokenStream2 {
+    match ir.shape {
+        StructShape::Named => {
+            let inits = ir.memento_fields().map(|field| {
+                let member = &field.member;
+                let value = build_from_expr_ir(field);
+                quote! { #member: #value }
+            });
+            quote! { Self { #(#inits),* } }
+        }
+        StructShape::Unnamed => {
+            let values = ir.memento_fields().map(build_from_expr_ir);
+            quote! { Self(#(#values),*) }
+        }
+        StructShape::Unit => {
+            quote! { Self }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn build_from_expr_ir(field: &FieldIr) -> TokenStream2 {
+    let member = &field.member;
+    match &field.strategy {
+        FieldStrategy::StoreAsSelf => quote! { value.#member },
+        FieldStrategy::StoreAsMemento(RecallPath::WholeType) => {
+            quote! { ::core::convert::From::from(value.#member) }
+        }
+        FieldStrategy::Skip => unreachable!("memento_fields() filters skipped fields"),
+    }
+}
+
+#[allow(dead_code)]
+fn build_from_where_clause(ir: &StructIr, env: &CodegenEnv) -> Option<syn::WhereClause> {
+    let recallable_trait = &env.recallable_trait;
+    let bounds: Vec<syn::WherePredicate> = ir
+        .recallable_params()
+        .flat_map(|ty| -> [syn::WherePredicate; 2] {
+            [
+                syn::parse_quote! { #ty: #recallable_trait },
+                syn::parse_quote! { #ty::Memento: ::core::convert::From<#ty> },
+            ]
+        })
+        .collect();
+    ir.extend_where_clause(&bounds)
 }
